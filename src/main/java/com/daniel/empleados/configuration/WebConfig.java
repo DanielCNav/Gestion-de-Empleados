008aa4a1a6a9c676adcc84dc535fcbbecb2aa9f7
DanielCNav <daniel.caballol.n@gmail.com>
2024-10-02 13:57:14 -0300

    Archivo WebConfig

--- src/main/java/com/daniel/empleados/configuration/WebConfig.java
+++ src/main/java/com/daniel/empleados/configuration/WebConfig.java
@@ -1,4 +1,16 @@
 package com.daniel.empleados.configuration;
 
-public class WebConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                //configurar localhost al puerto correspondiente
+                .allowedOrigins("http://localhost:5173")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+    }
 }
